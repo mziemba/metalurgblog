e4c893e876623f22822f9d8366bdb350bec8479a
Michal Ziemba <ziembal86@gmail.com>
2012-09-09 13:21:49 +0200

    Set locale

--- settings.py
+++ settings.py
@@ -22,9 +22,7 @@ DATABASES = {
 
 TIME_ZONE = 'Europe/Warsaw'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl'
 
 SITE_ID = 1
 
